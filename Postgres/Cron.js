ae083a0ea3a7bf10cbe5d93cc1031fcb104b3577
Gustavo <79329987+GustavoWornes@users.noreply.github.com>
2022-07-28 07:05:47 -0300

    Update Cron.js
    Limpando codigo dos console.log

--- Postgres/Cron.js
+++ Postgres/Cron.js
@@ -4,20 +4,14 @@ const IntegracaoCategorias =() => {
     
 cron.schedule("* 6 * * *",async ()=>{
     try {
-        console.log("teste")
         const {data} = await axios.get('http://localhost:3000/categorias')
         
         for(var i = 0;i <5;i++){
             const postgress = await axios.get(`http://localhost:3001/categorias/${data[i].codigo}`)
             if(postgress.data === null){
-                console.log("entrou no if",data[i])
                 const teste = await axios.post('http://localhost:3001/categorias',data[i])
             }
-            console.log("Data post:",postgress.data)
-      
-            console.log("Saiu do if",data[i].id)
         }
-        
         return 
        } catch (error) {
         return error
@@ -34,11 +28,9 @@ cron.schedule("5 6 * * *",async ()=>{
         for(var i=0;i < data.length;i++){
             const postgress = await axios.get(`http://localhost:3001/p/${data[i].codigo}`)
             if(postgress.data === null){
-                console.log("entrou no if",data[i])
                 const teste = await axios.post('http://localhost:3001/produtos',data[i])
-                console.log(teste)
+              
             }
-            console.log("Saiu do if",data[i].id)
      
         }
     } catch (error) {
